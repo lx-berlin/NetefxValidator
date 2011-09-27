27f32105b5ce394b75f639bcda733b661b1e8626
Zauberfisch <admin@zauberfisch.at>
2011-09-27 19:10:01 +0300

    API CHANGE: validate now returnes returned value of eval (using $return was stupid, but I was not aware that return in eval will actually return.

--- code/rules/NetefxValidatorRuleFUNCTION.php
+++ code/rules/NetefxValidatorRuleFUNCTION.php
@@ -8,7 +8,7 @@
 class NetefxValidatorRuleFUNCTION extends NetefxValidatorRule {
 		/**
 		 * Use a custom function to validate this field (the validator will pass 2 params to the custom function, first $data and as second the params you gave to the rule)
-		 * @example $code = 'if ($data["myField"] == "test") $return = true; else $return = false;'; $rule = new NetefxValidatorRuleFUNCTION("myField", 'the value must be test', null, $code);
+		 * @example $code = 'if ($data["myField"] == "test") return true; else return false;'; $rule = new NetefxValidatorRuleFUNCTION("myField", 'the value must be test', null, $code);
 		 * @example // this will call NetefxValidatorLibrary::min_number_checkboxes_checked($data, $args);<br>$class = 'NetefxValidatorLibrary'<br>$function = 'min_number_checkboxes_checked';<br>$args = array('field' => 'myField', 'min' => 5);<br>$rule = new NetefxValidatorRuleFUNCTION('myField', array($class, $method, $args), 'at least 5 checkboxes need to be checked');
 		 * @example // this will call $object->myValidationMethod($data, $args);<br> $object = new MyObject();<br>$function = 'myValidationMethod';<br>$args = array('fieldName' => 'myField', 'someOtherThing' => 'yay');<br>$rule = new NetefxValidatorRuleFUNCTION('myField', 'this field is required', null, array($object, $function, $args));
 		 * @example $function = create_function('$data,$args', 'if ($data["myField"] == "test" && $args["someOtherThing"]) return true; else return false;');<br>$args = array('someOtherThing' => true);<br>$rule = new NetefxValidatorRuleFUNCTION("myField", 'this field is required', null, array($function, $args));
@@ -40,9 +40,7 @@ class NetefxValidatorRuleFUNCTION extends NetefxValidatorRule {
 				if (count($args) > 2) $params['args'] = $args[2];
 			} else {
 				if (count($args) > 1) $params['args'] = $args[1];
-				$return = false;
-				eval($args[0]);
-				return $return;
+				return eval($args[0]);
 			}
 			return call_user_func_array($function, $params);
 		}
